92869b166852d3ecf0830a82b6863f3b000447f4
paulo-magls <paulomaga225@gmail.com>
2023-02-20 11:40:54 -0300

    segunda aula prática

---
+++ src/com/projeto/no/Main.java
@@ -0,0 +1,29 @@
+package com.projeto.no;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		No no1 = new No("Conteúdo nó 1");		
+		No no2 = new No("Conteúdo nó 2");	
+		no1.setProximoNo(no2);
+		
+		No no3 = new No("Conteúdo nó 3");
+		no2.setProximoNo(no3);
+		
+		No no4 = new No("Conteúdo nó 4");
+		no3.setProximoNo(no4);
+		
+		//no1->no2->no3->no4->null
+		
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		System.out.println(no2);
+		
+		System.out.println("---------------------------");
+		
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+	}
+}
