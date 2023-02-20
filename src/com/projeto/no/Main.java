8045f1a593a0edd3c4f5ac4f990d01860eb8f2d7
paulo-magls <paulomaga225@gmail.com>
2023-02-20 11:29:11 -0300

    refatorando a classe Nó (terceira aula prática)

--- src/com/projeto/no/Main.java
+++ src/com/projeto/no/Main.java
@@ -3,14 +3,14 @@ package com.projeto.no;
 public class Main {
 	public static void main(String[] args) {
 		
-		No no1 = new No("Conteúdo nó 1");		
-		No no2 = new No("Conteúdo nó 2");	
+		No<String> no1 = new No<>("Conteúdo nó 1");		
+		No<String> no2 = new No<>("Conteúdo nó 2");
 		no1.setProximoNo(no2);
 		
-		No no3 = new No("Conteúdo nó 3");
+		No<String> no3 = new No<>("Conteúdo nó 3");
 		no2.setProximoNo(no3);
 		
-		No no4 = new No("Conteúdo nó 4");
+		No<String> no4 = new No<>("Conteúdo nó 4");
 		no3.setProximoNo(no4);
 		
 		//no1->no2->no3->no4->null
--- src/com/projeto/no/No.java
+++ src/com/projeto/no/No.java
@@ -1,28 +1,28 @@
 package com.projeto.no;
 
-public class No {
-	private String conteudo;
-	private No proximoNo;
+public class No<T> {
+	private T conteudo;
+	private No<T> proximoNo;
 
-	public No(String conteudo) {
+	public No(T conteudo) {
 		super();
 		this.proximoNo = null;
 		this.conteudo = conteudo;
 	}
 
-	public String getConteudo() {
+	public T getConteudo() {
 		return conteudo;
 	}
 
-	public void setConteudo(String conteudo) {
+	public void setConteudo(T conteudo) {
 		this.conteudo = conteudo;
 	}
 
-	public No getProximoNo() {
+	public No<T> getProximoNo() {
 		return proximoNo;
 	}
 
-	public void setProximoNo(No proximoNo) {
+	public void setProximoNo(No<T> proximoNo) {
 		this.proximoNo = proximoNo;
 	}
 
