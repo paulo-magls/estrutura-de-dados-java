562ea0af096c0200295c0bcc00b641fab74c1187
paulo-magls <paulomaga225@gmail.com>
2023-02-21 12:09:28 -0300

    implementando a classe ListaCircular

---
+++ src/com/projeto/listacircular/Main.java
@@ -0,0 +1,32 @@
+package com.projeto.listacircular;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ListaCircular<String> minhaListaCircular = new ListaCircular<>();
+		
+		minhaListaCircular.add("c0");
+		System.out.println(minhaListaCircular);
+		
+		minhaListaCircular.remove(0);
+		System.out.println(minhaListaCircular);
+		
+		minhaListaCircular.add("c1");
+		System.out.println(minhaListaCircular);
+		
+		minhaListaCircular.add("c2");
+		minhaListaCircular.add("c3");
+		System.out.println(minhaListaCircular);
+		
+//		System.out.println(minhaListaCircular.get(0));
+//		System.out.println(minhaListaCircular.get(1));
+//		System.out.println(minhaListaCircular.get(2));
+//		System.out.println(minhaListaCircular.get(3));
+//		System.out.println(minhaListaCircular.get(4));
+		
+		for(int i = 0; i < 20; i++) {
+			System.out.println(minhaListaCircular.get(i));
+		}
+	}
+
+}
