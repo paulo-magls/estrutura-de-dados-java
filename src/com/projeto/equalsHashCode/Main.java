6e92769f4a9d519842217f1e12adc8751855682f
paulo-magls <paulomaga225@gmail.com>
2023-02-22 08:50:59 -0300

    implementando os métodos equals e hashCode

---
+++ src/com/projeto/equalsHashCode/Main.java
@@ -0,0 +1,26 @@
+package com.projeto.equalsHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<Carro> listaCarros = new ArrayList<>();
+		
+		listaCarros.add(new Carro("Ford"));
+		listaCarros.add(new Carro("Chevrolet"));
+		listaCarros.add(new Carro("Volkswagen"));
+		
+		System.out.println(listaCarros.contains(new Carro("Ford")));
+		System.out.println(new Carro("Ford").hashCode());
+		System.out.println(new Carro("Ford1").hashCode());
+		
+		Carro carro1 = new Carro("Ford");
+		Carro carro2 = new Carro("Cevrolet");
+		
+		System.out.println(carro1.equals(carro2));
+	}
+
+}
